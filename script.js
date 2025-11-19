ea9c331ea1c34790c187d7a3e2921188cec9ec1c
Jhonatan1221 <a3521110214@alumno.uttehuacan.edu.mx>
2025-11-19 16:17:25 -0600

    fase1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+function add(a, b) {
+    return parseFloat(a) + parseFloat(b);
+}
+
+function subtract(a, b) {
+    return parseFloat(a) - parseFloat(b);
+}
+
+function multiply(a, b) {
+    return parseFloat(a) * parseFloat(b);
+}
+
+function divide(a, b) {
+    return parseFloat(a) / parseFloat(b);
+}
+
+let number1 = "10";
+let number2 = "20";
+
+console.log(add(number1, number2));        
+console.log(subtract(number1, number2));       
+console.log(multiply(number1, number2));  
+console.log(divide(number1, number2));
